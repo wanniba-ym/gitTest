fb5f9835a6b57cd7b57a80db7a934dea378ee0f7
wanniba-ym <3302851200@qq.com>
2021-05-15 14:45:34 +0800

    第一次提交

---
+++ src/pers/ym/test/Entity.java
@@ -0,0 +1,15 @@
+package pers.ym.test;
+
+public class Entity {
+    private Integer code;
+    private String message;
+
+    public void method1(){
+
+    }
+
+    public void method2() {
+
+    }
+
+}
---
+++ src/pers/ym/test/TestDemo2.java
@@ -0,0 +1,9 @@
+package pers.ym.test;
+
+public class TestDemo2 {
+
+    public void test1(){
+        System.out.println("进行git测试");
+    }
+
+}
